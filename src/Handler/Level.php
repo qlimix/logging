fbfd13624548171f84aefeac2a621962dd092570
frank <frank@qlimix.net>
2019-03-13 21:07:30 +0100

    general update

--- src/Handler/Level.php
+++ src/Handler/Level.php
@@ -50,81 +50,51 @@ final class Level
     /** @var string */
     private $level;
 
-    /**
-     * @param string $level
-     */
     public function __construct(string $level)
     {
         $this->level = $level;
     }
 
-    /**
-     * @return Level
-     */
     public static function createEmergency(): Level
     {
         return new self(self::LEVEL_EMERGENCY);
     }
 
-    /**
-     * @return Level
-     */
     public static function createAlert(): Level
     {
         return new self(self::LEVEL_ALERT);
     }
 
-    /**
-     * @return Level
-     */
     public static function createCritical(): Level
     {
         return new self(self::LEVEL_CRITICAL);
     }
 
-    /**
-     * @return Level
-     */
     public static function createError(): Level
     {
         return new self(self::LEVEL_ERROR);
     }
 
-    /**
-     * @return Level
-     */
     public static function createWarning(): Level
     {
         return new self(self::LEVEL_WARNING);
     }
 
-    /**
-     * @return Level
-     */
     public static function createNotice(): Level
     {
         return new self(self::LEVEL_NOTICE);
     }
 
-    /**
-     * @return Level
-     */
     public static function createInfo(): Level
     {
         return new self(self::LEVEL_INFO);
     }
 
-    /**
-     * @return Level
-     */
     public static function createDebug(): Level
     {
         return new self(self::LEVEL_DEBUG);
     }
 
-    /**
-     * @return string
-     */
     public function getLevel(): string
     {
         return $this->level;
--- src/Handler/LogHandlerInterface.php
+++ src/Handler/LogHandlerInterface.php
@@ -5,10 +5,7 @@ namespace Qlimix\Log\Handler;
 interface LogHandlerInterface
 {
     /**
-     * @param Channel $channel
-     * @param Level $level
-     * @param string $message
-     * @param array $context
+     * @param mixed[] $context
      */
     public function log(Channel $channel, Level $level, string $message, array $context): void;
 }
